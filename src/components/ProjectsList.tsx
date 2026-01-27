1f7e8f05247fa5936a4a78a54feec3e14065154d
MikeL538 <mikel538.work@gmail.com>
2026-01-27 13:02:57 +0100

    feat: Projects page implemended, first prototype of page visuals

---
+++ src/components/ProjectsList.tsx
@@ -0,0 +1,62 @@
+import projects from "../assets/projects.json";
+import icons from "../assets/icons.svg";
+
+export default function ProjectsList({
+  onImageClick,
+}: {
+  onImageClick: (project: any) => void;
+}) {
+  //   const projectsJson = projects.filter((p) => p.featured);
+
+  return (
+    <section className="projects">
+      <div className="projects__container">
+        <h2 className="projects__title">Projects</h2>
+        <ul className="projects__list">
+          {projects.map((project) => (
+            <li key={project.id} className="projects__item">
+              <div className="projects__list-container">
+                <img
+                  className="projects__image"
+                  src={project.image}
+                  alt={project.title}
+                  onClick={() => onImageClick(project)}
+                />
+                <div className="projects__list-text">
+                  <h3 className="projects__name">{project.title}</h3>
+                  <p className="projects__description">{project.description}</p>
+
+                  <ul className="projects__list-tech">
+                    {project.tech.map((tech) => (
+                      <li key={tech}>{tech}</li>
+                    ))}
+                  </ul>
+                  <div className="projects__links-container">
+                    <a
+                      className="projects__links"
+                      target="_blank"
+                      href={project.live}
+                    >
+                      <svg className="projects__links-svg">
+                        <use href={`${icons}#icon-www`} />
+                      </svg>
+                    </a>
+                    <a
+                      className="projects__links"
+                      target="_blank"
+                      href={project.github}
+                    >
+                      <svg className="projects__links-svg">
+                        <use href={`${icons}#icon-github`} />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
+import ImageModal from "../components/ImageModal";
+import ProjectsList from "../components/ProjectsList";
+
 export default function Projects() {
+  const [activeProject, setActiveProject] = useState<any>(null);
+
   return (
     <>
-      <h1>Projects</h1>
+      <div className="projects">
+        <ProjectsList onImageClick={setActiveProject} />
+        <ImageModal
+          project={activeProject}
+          onClose={() => setActiveProject(null)}
+        />
+      </div>
     </>
   );
 }
