398c28300db0a8719944d2e6b474064c6fcf4ac9
MikeL538 <mikel538.work@gmail.com>
2026-01-29 20:15:18 +0100

    fix: vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Micha-Lipiak-Fullstack-Developer-Portfolio",
+  base: "/Micha-Lipiak-Fullstack-Developer-Portfolio/",
 });
